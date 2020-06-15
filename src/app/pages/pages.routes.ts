83dcd11c1db084e23b6c570e9c9ca0b2b4aad321
rdpineda <pinedahernandez@gmail.com>
2020-06-15 12:45:53 -0500

    actualizado15062020

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
-import { Graficas1Component } from './graficas1/graficas1.component';
+
 import { AccoutSettingsComponent } from './accout-settings/AccoutSettingsComponent';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
@@ -25,7 +25,7 @@ const pagesRoutes: Routes = [
             data: { titulo: 'Dashboard' }
         },
         { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' }},
-        { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Graficas' }},
+      
         { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' }},
         { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RXjs' }},
         { path: 'account-settings', component: AccoutSettingsComponent, data: { titulo: 'Ajuste del Tema' }},
